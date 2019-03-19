fc4e100b78ed015bb958cd8e0e04764a20c4d2ab
js-commander <afkfivesecond@gmail.com>
2019-03-19 20:02:54 +0800

    fix style, prepare to merge

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { InputData } from "./interfaces"
 
 const doc = document as any;
 
+window.Chart = Chart;
+
 doc.createChart = (canvas: HTMLCanvasElement, data: InputData) => {
   return new Chart(canvas, data);
 }
